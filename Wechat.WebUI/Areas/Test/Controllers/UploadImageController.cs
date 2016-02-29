e3579d3faa1db9379db867aa66ca0b1b0fb7cd01
yanliping <yanliping@hreec.com>
2016-02-29 17:16:24 +0800

    添加上传图片代码

---
+++ Wechat.WebUI/Areas/Test/Controllers/UploadImageController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wechat.WebUI.Areas.Test.Controllers
+{
+    public class UploadImageController : Controller
+    {
+        //
+        // GET: /Test/UploadImage/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            #region 上传到本地
+            var strogeFolder = string.Format("/Upload/Header/{0}/", DateTime.Now.ToString("yyyy/MM/dd"));
+
+            var networkPath = strogeFolder;
+            var physicalPath = string.Format("{0}{1}", System.AppDomain.CurrentDomain.BaseDirectory, strogeFolder);
+
+            var fileName = string.Format("{0}.{1}", Guid.NewGuid().ToString("N"), "jpg");
+
+            string header = YLP.Tookit.Helper.UploadImageHelper.Upload(networkPath, physicalPath, fileName);
+
+            if (!string.IsNullOrEmpty(header))
+            {
+                //TODO:保存到数据库
+
+                return Content("上传成功");
+            }
+            return Content("上传失败");
+            #endregion
+        }
+    }
+}
---
+++ Wechat.WebUI/HttpHandler/UploadImage.ashx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wechat.WebUI.HttpHandler
+{
+    /// <summary>
+    /// UploadImage 的摘要说明
+    /// </summary>
+    public class UploadImage : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            //注释：利用一般处理程序进行上传的话是没有办法进行保存到数据库操作的
+
+            var strogeFolder = string.Format("/Upload/Header/{0}/", DateTime.Now.ToString("yyyy/MM/dd"));
+
+            var networkPath = strogeFolder;
+            var physicalPath = string.Format("{0}{1}", System.AppDomain.CurrentDomain.BaseDirectory, strogeFolder);
+
+            var fileName = string.Format("{0}.{1}", Guid.NewGuid().ToString("N"), "jpg");
+
+            YLP.Tookit.Helper.UploadImageHelper.Upload(networkPath, physicalPath, fileName);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
