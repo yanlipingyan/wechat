7ecd6af646f0a285c2db4bf062accc1f4ea8fd41
yanlipingyan <759001549@qq.com>
2016-03-06 08:55:52 +0800

    添加layer插件

---
+++ Wechat.WebUI/ApiControllers/ApiBasicController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Wechat.WebUI.ApiControllers
+{
+    public class ApiBasicController : ApiController
+    {
+        #region Api返回结果
+        protected class ApiResult
+        {
+            public bool Flag { get; set; }
+
+            public object Content { get; set; }
+
+            public string RedirectUrl { get; set; }
+        }
+        protected ApiResult Success(object message)
+        {
+            return new ApiResult { Flag = true, Content = message };
+        }
+
+        protected ApiResult Faild(string message)
+        {
+            return new ApiResult { Flag = false, Content = message };
+        }
+
+        protected ApiResult Result(bool flag)
+        {
+            if (flag)
+                return Success("操作成功");
+            return Faild("操作失败");
+        }
+
+        //public ApiResult Result<T>(bool flag) 
+        //{
+
+        //}
+
+
+        /// <summary>
+        /// model验证失败
+        /// </summary>
+        /// <returns></returns>
+        protected ApiResult ModelValidError()
+        {
+            var errorInfos = ModelState.Where(x => x.Value.Errors.Count > 0);
+            var errorInfo = errorInfos.SelectMany(x => x.Value.Errors).ToArray()[0].ErrorMessage;
+
+            return new ApiResult { Flag = false, Content = errorInfo };
+        }
+        #endregion
+    }
+}
---
+++ Wechat.WebUI/ApiControllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Wechat.WebUI.ApiControllers
+{
+    public class TestController : ApiBasicController
+    {
+        [HttpGet]
+        public object Get()
+        {
+            return Success("This id Get Method");
+        }
+
+        [HttpPost]
+        public object Post([FromBody]object content)
+        {
+            return Success(content);
+        }
+
+        [HttpPut]
+        public object Put(string id, [FromBody]object content)
+        {
+            return Success(content);
+        }
+
+        [HttpDelete]
+        public object Delete(string id)
+        {
+            return Success("This is Post Method");
+        }
+    }
+}
