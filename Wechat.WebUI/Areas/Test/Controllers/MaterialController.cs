8db5106fe4a91ecd53e5716fd04f13f93cf7ea6a
yanliping <yanliping@hreec.com>
2016-03-18 17:47:09 +0800

    no message

--- Wechat.WebUI/Areas/Test/Controllers/MaterialController.cs
+++ Wechat.WebUI/Areas/Test/Controllers/MaterialController.cs
@@ -4,8 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using Wechat.API;
 using Wechat.API.Models;
+using YLP.Tookit.Helper;
 
 namespace Wechat.WebUI.Areas.Test.Controllers
 {
@@ -14,49 +16,63 @@ namespace Wechat.WebUI.Areas.Test.Controllers
         //
         // GET: /Test/Material/
 
-        string media_id = "xuRYVkC2dDDv6h4zu_dq2QiMRyvCzXFoCboYdDuyTmMRLkv8G_QXYc6DcxDFapEG";
-
         public ActionResult Index()
         {
-            return Content(Material.GetTemporaryMedia(ApiModel.AppID, ApiModel.AppSecret, media_id));
+            return View();
         }
 
-        public ActionResult Download()
+        [HttpPost]
+        public ActionResult Download(string media_ids)
         {
+            string netPaths = "";
             using (MemoryStream ms = new MemoryStream())
             {
-                string folder = string.Format("upload/material/{0}", DateTime.Now.ToString("yyyyMMdd"));
-                string filename = string.Format("{0}.jpg", DateTime.Now.Ticks);
-                string physicalPath = System.Web.HttpContext.Current.Request.PhysicalApplicationPath + folder;
-                string netPath = "/" + folder + "/" + filename;
-
-                //如果日志目录不存在就创建
-                if (!Directory.Exists(physicalPath))
+                foreach (var media_id in media_ids.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
                 {
-                    Directory.CreateDirectory(physicalPath);
-                }
+                    string folder = string.Format("upload/material/{0}", DateTime.Now.ToString("yyyyMMdd"));
+                    string filename = string.Format("{0}.jpg", IDHelper.Id32);
+                    string physicalPath = System.Web.HttpContext.Current.Request.PhysicalApplicationPath + folder;
+                    string netPath = "/" + folder + "/" + filename;
 
-                if (string.IsNullOrEmpty(media_id))
-                    return Content("media_id为空");
-
-                Material.GetTemporaryMedia(ApiModel.AppID, ApiModel.AppSecret, media_id, ms);
-
-                //保存到文件
-                var fileName = physicalPath + "/" + filename;
-                using (FileStream fs = new FileStream(fileName, FileMode.Create))
-                {
-                    ms.Position = 0;
-                    byte[] buffer = new byte[1024];
-                    int bytesRead = 0;
-                    while ((bytesRead = ms.Read(buffer, 0, buffer.Length)) != 0)
+                    //如果日志目录不存在就创建
+                    if (!Directory.Exists(physicalPath))
                     {
-                        fs.Write(buffer, 0, bytesRead);
+                        Directory.CreateDirectory(physicalPath);
                     }
-                    fs.Flush();
-                }
 
-                return View((object)netPath);
+                    if (string.IsNullOrEmpty(media_id))
+                        return Content("media_id为空");
+
+                    Material.GetTemporaryMedia(ApiModel.AppID, ApiModel.AppSecret, media_id, ms);
+
+                    //保存到文件
+                    var fileName = physicalPath + "/" + filename;
+
+                    //FileStream fs = new FileStream(fileName, FileMode.Create);
+                    //ms.Position = 0;
+                    //byte[] buffer = new byte[1024];
+                    //int bytesRead = 0;
+                    //while ((bytesRead = ms.Read(buffer, 0, buffer.Length)) != 0)
+                    //{
+                    //    fs.Write(buffer, 0, bytesRead);
+                    //}
+                    //fs.Flush();
+                    using (FileStream fs = new FileStream(fileName, FileMode.Create))
+                    {
+                        ms.Position = 0;
+                        byte[] buffer = new byte[1024];
+                        int bytesRead = 0;
+                        while ((bytesRead = ms.Read(buffer, 0, buffer.Length)) != 0)
+                        {
+                            fs.Write(buffer, 0, bytesRead);
+                        }
+                        fs.Flush();
+                    }
+                    netPaths += netPath + ",";
+                }
             }
+
+            return Content(new JavaScriptSerializer().Serialize(new { Flag = true, Content = netPaths }));
         }
 
     }
