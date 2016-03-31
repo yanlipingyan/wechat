21d57bb84b94f4cd9f05091f5c2840cae794019c
yanliping <yanliping@hreec.com>
2016-03-31 18:07:02 +0800

    测试七牛

--- YLP.Tookit/Component/QiNiu.cs
+++ YLP.Tookit/Component/QiNiu.cs
@@ -4,34 +4,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using YLP.Tookit.Helper;
 
 namespace YLP.Tookit.Component
 {
     public class QiNiu
     {
-        private void Upload()
+        /// <summary>
+        /// 上传文件，普通上传
+        /// </summary>
+        /// <param name="bucket_name">设置上传的空间</param>
+        /// <param name="filePath">上传文件的路径</param>
+        public void Upload(string bucket_name, string filePath)
         {
             IOClient target = new IOClient();
             PutExtra extra = new PutExtra();
-            //设置上传的空间
-            String bucket = "bucket_name";
             //设置上传的文件的key值
             String key = "yourdefinekey";
 
             //普通上传,只需要设置上传的空间名就可以了,第二个参数可以设定token过期时间
-            PutPolicy put = new PutPolicy(bucket, 3600);
+            PutPolicy put = new PutPolicy(bucket_name, 3600);
 
             //调用Token()方法生成上传的Token
             string upToken = put.Token();
-            //上传文件的路径
-            String filePath = "/.../...";
+            Log4NetHelper.Error(string.Format("upToken:{0}", upToken));
 
             //调用PutFile()方法上传
             PutRet ret = target.PutFile(upToken, key, filePath, extra);
+
             //打印出相应的信息
-            Console.WriteLine(ret.Response.ToString());
-            Console.WriteLine(ret.key);
-            Console.ReadLine();
+            Log4NetHelper.Error(ret.Response.ToString());
+            Log4NetHelper.Error(ret.key);
+            //Console.WriteLine(ret.Response.ToString());
+            //Console.WriteLine(ret.key);
+            //Console.ReadLine();
         }
     }
 }
