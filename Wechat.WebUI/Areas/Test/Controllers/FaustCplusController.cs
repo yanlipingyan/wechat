aa2fe77d0eb7346254162510503bb18b0b43514b
yanliping <yanliping@hreec.com>
2016-04-06 17:51:37 +0800

    修改上传图片后保存的方式

--- Wechat.WebUI/Areas/Test/Controllers/FaustCplusController.cs
+++ Wechat.WebUI/Areas/Test/Controllers/FaustCplusController.cs
@@ -31,7 +31,7 @@ namespace Wechat.WebUI.Areas.Test.Controllers
             byte[] ms_b = new byte[fh];
             System.Web.HttpContext.Current.Request.InputStream.Read(ms_b, 0, ms_b.Length);
 
-            header = QiNiu.GetDownloadUrl("7xqce5.com1.z0.glb.clouddn.com", "teamopf-test", QiNiu.Upload("teamopf-test", ms_b, "jpg"));
+            header = QiNiu.GetDownloadUrl("www.liblog.cn", "wechat", QiNiu.Upload("wechat", ms_b, "jpg"));
             #endregion
 
             #region 上传到本地
