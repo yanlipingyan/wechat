6e26a13d29ac6ace526bdd01eea7dcd86d7112b8
yanlipingyan <759001549@qq.com>
2016-08-06 23:02:58 +0800

    添加高德地图

---
+++ Wechat.WebUI/Areas/Test/Controllers/GaoDeMapController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wechat.WebUI.Areas.Test.Controllers
+{
+    public class GaoDeMapController : Controller
+    {
+        //
+        // GET: /Test/GaoDeMap/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
